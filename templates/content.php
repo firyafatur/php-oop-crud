563132979c2c394837aac1ca759bc2f8bf07037f
firyafatur <faturgames19@gmail.com>
2020-10-24 22:28:23 +0700

    membuat navbar membuat content membuat footer membuat header

---
+++ templates/content.php
@@ -0,0 +1,8 @@
+<div class="row-md-12 p-3">
+   <div class="col-md-12">
+      <h4 class="text text-center">PHP OOP CRUD</h4>
+   </div>
+   <div class="col-lg-6">
+      <h4 class="text text-center text-primary">Daftar Data Pengguna</h4>
+   </div>
+</div>
---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP-OOP-CRUD</title>
+
+   <!-- Dependency CSS -->
+   <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+   <!-- DataTables -->
+   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+
+</head>
+
+<body>
